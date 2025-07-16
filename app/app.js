e8899e1bc6f0dc0dc9ceabd843302c848551d799
yawar-33 <yawarali339@gmail.comm>
2025-07-16 18:29:01 +0500

    testing initial commit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const serverConfig = require('./config/server.config');
 
 // Set the port based on environment or configuration
 const PORT = process.env.PORT || serverConfig.PORT;
-
+// testing
 // Start the server
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
